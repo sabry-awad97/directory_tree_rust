399ec2d4605be2cb9c1472aa436c754746ce328f
Sabry Awad <dr.sabry1997@gmail.com>
2023-02-18 09:55:09 +0200

    enhanced version

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,89 @@
 use std::fs;
 use std::path::Path;
 
-fn print_directory_tree(path: &Path, prefix: &str) {
-    let dir_contents = fs::read_dir(path).unwrap().collect::<Vec<_>>();
+enum EntryType {
+    File,
+    Directory,
+}
 
-    for (index, entry) in dir_contents.iter().enumerate() {
-        let entry = entry.as_ref().unwrap();
-        let entry_path = entry.path();
-        let entry_name = entry_path.file_name().unwrap().to_str().unwrap();
+struct Entry {
+    name: String,
+    entry_type: EntryType,
+}
 
-        // Print the prefix for this entry, with appropriate characters depending on whether it's the last entry in the directory
-        let (line_char, branch_char) = if index == dir_contents.len() - 1 {
-            ("└", " ")
+#[derive(Clone)]
+enum LineType {
+    Single,
+    Last,
+}
+
+#[derive(Clone)]
+struct Line {
+    line_type: LineType,
+    indent_level: usize,
+}
+
+fn print_directory_tree(path: &Path, indent_level: usize, line: Line) {
+    let mut entries: Vec<Entry> = Vec::new();
+    for entry in fs::read_dir(path).unwrap() {
+        let entry = entry.unwrap();
+        let name = entry.file_name().into_string().unwrap();
+        let entry_type = if entry.path().is_dir() {
+            EntryType::Directory
         } else {
-            ("├", "│")
+            EntryType::File
         };
-        println!("{}{}── {}", prefix, line_char, entry_name);
+        entries.push(Entry { name, entry_type });
+    }
 
-        // Recursively print the contents of any subdirectories
-        if entry_path.is_dir() {
-            let new_prefix = format!("{}{}   ", prefix, branch_char);
-            print_directory_tree(&entry_path, &new_prefix);
+    for (index, entry) in entries.iter().enumerate() {
+        let is_last_entry = index == entries.len() - 1;
+        let line_char = match (line.clone().line_type, is_last_entry) {
+            (LineType::Single, false) => "├",
+            (LineType::Single, true) => "└",
+            (LineType::Last, false) => "│",
+            (LineType::Last, true) => " ",
+        };
+        let prefix = "│   ".repeat(indent_level);
+        let entry_line = format!("{}{}── {}", prefix, line_char, entry.name);
+
+        println!("{}", entry_line);
+
+        let new_indent_level = indent_level + 1;
+        let new_line = match (line.clone().line_type, is_last_entry) {
+            (LineType::Single, false) => Line {
+                line_type: LineType::Single,
+                indent_level: new_indent_level,
+            },
+            (LineType::Single, true) => Line {
+                line_type: LineType::Last,
+                indent_level: new_indent_level,
+            },
+            (LineType::Last, false) => Line {
+                line_type: LineType::Single,
+                indent_level: new_indent_level,
+            },
+            (LineType::Last, true) => Line {
+                line_type: LineType::Last,
+                indent_level: new_indent_level,
+            },
+        };
+
+        if let EntryType::Directory = entry.entry_type {
+            let sub_path = path.join(&entry.name);
+            print_directory_tree(&sub_path, new_indent_level, new_line);
         }
     }
 }
 
 fn main() {
     let path = Path::new(".");
-    print_directory_tree(&path, "");
+    print_directory_tree(
+        path,
+        0,
+        Line {
+            line_type: LineType::Last,
+            indent_level: 0,
+        },
+    );
 }
